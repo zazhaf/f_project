9c95697277441f1373614237589d47c71fb5feec
rafi <rafirach@gmail.com>
2023-08-19 01:39:25 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/ 
 
 Route::get('/home', function () {
     return view('main_page/home');
